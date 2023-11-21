691fd08cac5084c36a16603f243026aa03c73eee
Dmitriy Pervin <theforner@gmail.com>
2023-11-21 22:31:52 +0500

    Add DTO

--- src/utils/fetchSchedule.ts
+++ src/utils/fetchSchedule.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
+import { EventDto } from './event.dto';
 
 dotenv.config();
 
-export async function fetchSchedule() {
+export async function fetchSchedule(): Promise<EventDto[]> {
   const res = await fetch(`${process.env.PARSER_URL}`, {
     method: 'GET',
     headers: {
